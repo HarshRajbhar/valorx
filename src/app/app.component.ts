e4c71972424370463954116742f5944d911d7e31
Harsh kumar rajbhar <hkrajbhar2002@gmail.com>
2024-11-08 23:42:56 +0530

    all compo created And service created.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormCrudComponent } from './form-crud/form-crud.component';
+import { ListDataComponent } from './list-data/list-data.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListDataComponent, FormCrudComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'valorx';
